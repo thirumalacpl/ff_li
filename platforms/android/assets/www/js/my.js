1a8c8d09aecf46e3f63b00b9c8a5bbab4f781f8d
kalyan <kalyan@staging.eimpressive.com>
2016-02-29 16:14:44 +0530

    ff_li0123as

--- platforms/android/assets/www/js/my.js
+++ platforms/android/assets/www/js/my.js
@@ -69,6 +69,66 @@ $.ajax({
 });
 });*/
 
+$(document).off('click', '#ssy').on('click', '#ssy', function() {
+//alert(region+'refresh');
+
+db.transaction(function(transaction) {
+transaction.executeSql('SELECT * FROM mydata', [], function (tx, results) { 
+var key = "";
+       var pair="<tr><th data-priority=\"1\"><center>Id</center></th><th data-priority=\"1\"><center>Name</center></th><th data-priority=\"2\"><center>Email</center></th><th data-priority=\"3\"><center>Event</center></th><th><center>Update</center></th><th><center>Delete</center></th></tr>";
+       var i=0;
+       var len = results.rows.length, i;
+       for (i=0; i<=len-1; i++) {
+         key = results.rows.item(i).name;
+         eventa = results.rows.item(i).eventa;
+         id = results.rows.item(i).id;
+         pair += "<tr><td><center>"+id+"</center></td><td><center>"+key+"</center></td><td><center>"+results.rows.item(i).email+"</center></td><td><center>"+eventa+"</center></td><td><a class=\"update\" href=\"#myPopupDialog\"  data-custom="+"'"+ id+ "'" +"data-rel=\"popup\" data-position-to=\"window\" data-transition=\"pop\"><center><i class='fa fa-pencil-square-o'></i></center></a></td><td><a  id=\"delete\" data=\""+id+"\"><center><i class='fa fa-trash'></i></center></a></td></tr>";
+       alert(key+'keey for');
+           alert(eventa+'eventa for');
+               alert(id+'id for');
+emaill='asd';
+$.ajax({url: 'http://staging.eimpressive.com/slim-four/offline.php?id='+id+'&key='+key+'&emaill='+emaill+'&eventa='+eventa,
+  data:$('#new').serialize(),
+  type: 'post',                   
+  async: 'true',
+  crossDomain: true,
+  dataType: 'json',
+  beforeSend: function() {
+  },
+  complete: function() {
+  },
+  success: function (result) {
+    console.log('searchlpa ' +result);
+    if(result[0]){
+      $("#popupsearchmade").popup("open");
+alert('sessionStorage return');
+sessionStorage.setItem("collectionArray",JSON.stringify(result[0]));
+// alert(region+'refresh new regionArray_array');
+$.mobile.loading().hide();
+}else {
+  alert('No Data Found for the search record'); 
+}
+
+return false;
+},
+error: function (request,error) {
+// This callback function will trigger on unsuccessful action     
+console.log(request);
+console.log(error);  
+
+alert('Network error has occurred please try again!');
+}
+});
+
+       }
+      
+
+}, null);
+});
+
+
+});
+
 $(document).off('click', '#syncc').on('click', '#syncc', function() {
 //alert(region+'refresh');
 alert(key+'key');
