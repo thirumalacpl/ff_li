3871448898086e31fed9d09e2d6eef1e43ce96ab
kalyan <kalyan@staging.eimpressive.com>
2016-02-29 15:49:50 +0530

    ff_li0

--- platforms/android/assets/www/js/my.js
+++ platforms/android/assets/www/js/my.js
@@ -45,19 +45,8 @@ $("#myTable").html(pair);
 });
 }
 
-$(document).on('click', '#syncc', function(){
+/*$(document).on('click', '#syncc', function(){
 
-db.transaction(function(transaction) {
-transaction.executeSql('SELECT * FROM mydata', [], function (tx, results) { 
-var key = "";
-       var pair="<tr><th data-priority=\"1\"><center>Id</center></th><th data-priority=\"1\"><center>Name</center></th><th data-priority=\"2\"><center>Email</center></th><th data-priority=\"3\"><center>Event</center></th><th><center>Update</center></th><th><center>Delete</center></th></tr>";
-       var i=0;
-       var len = results.rows.length, i;
-       for (i=0; i<=len-1; i++) {
-         key = results.rows.item(i).name;
-         eventa = results.rows.item(i).eventa;
-         id = results.rows.item(i).id;
-         pair += "<tr><td><center>"+id+"</center></td><td><center>"+key+"</center></td><td><center>"+results.rows.item(i).email+"</center></td><td><center>"+eventa+"</center></td><td><a class=\"update\" href=\"#myPopupDialog\"  data-custom="+"'"+ id+ "'" +"data-rel=\"popup\" data-position-to=\"window\" data-transition=\"pop\"><center><i class='fa fa-pencil-square-o'></i></center></a></td><td><a  id=\"delete\" data=\""+id+"\"><center><i class='fa fa-trash'></i></center></a></td></tr>";
        $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
   options.async = true;
 });
@@ -75,14 +64,51 @@ $.ajax({
   dataType: 'json',
   error: onError
 });
+});*/
 
-       }
-       if (pair == "<tr><th>Name</th><th>Day</th><th>Event</th></tr>") {
-         pair += "<tr><td><i>empty</i></td><td><i>empty</i></td><td><i>empty</i></td></tr>";
-       }
-/*$("#myTable").html(pair);*/
-}, null);
+$(document).off('click', '#syncc').on('click', '#syncc', function() {
+//alert(region+'refresh');
+alert(key+'key');
+alert(eventa+'eventa');
+alert(id+'id');
+var id=1;
+var key='key';
+var emaill='emaill';
+var eventa='eventa';
+
+$.ajax({url: 'http://staging.eimpressive.com/slim-four/offline.php?id='+id+'&key='+key+'&emaill='+emaill+'&eventa='+eventa,
+  data:$('#new').serialize(),
+  type: 'post',                   
+  async: 'true',
+  crossDomain: true,
+  dataType: 'json',
+  beforeSend: function() {
+  },
+  complete: function() {
+  },
+  success: function (result) {
+    console.log('searchlpa ' +result);
+    if(result[0]){
+      $("#popupsearchmade").popup("open");
+alert('sessionStorage return');
+sessionStorage.setItem("collectionArray",JSON.stringify(result[0]));
+// alert(region+'refresh new regionArray_array');
+$.mobile.loading().hide();
+}else {
+  alert('No Data Found for the search record'); 
+}
+
+return false;
+},
+error: function (request,error) {
+// This callback function will trigger on unsuccessful action     
+console.log(request);
+console.log(error);  
+
+alert('Network error has occurred please try again!');
+}
 });
+
 });
 
 $(document).on('click', '#delete', function(){
